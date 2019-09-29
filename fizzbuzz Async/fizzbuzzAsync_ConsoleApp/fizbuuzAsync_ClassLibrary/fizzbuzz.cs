5399a8964dad919bdc0569d59478bd06bb788090
SergBoiko <54549436+SergBoiko@users.noreply.github.com>
2019-09-29 15:49:11 +0300

    Add files via upload

---
+++ fizzbuzz Async/fizzbuzzAsync_ConsoleApp/fizbuuzAsync_ClassLibrary/fizzbuzz.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizbuuzAsync_ClassLibrary
+{
+    public static class FizzBuzz
+    {
+        private static string Transform(int x)
+        {
+            int number3 = x % 3;
+            int number5 = x % 5;
+            string result = "";
+
+            if (number3 == 0)
+            {
+                result += "Fizz";
+            }
+            if (number5 == 0)
+            {
+                result += "Buzz";
+            }
+            if (number3 != 0 && number5 != 0)
+            {
+                result = x.ToString();
+            }
+
+            return result;
+        }
+
+        public static List<string> Sync(List<int> list)
+        {
+            List<string> stringList = new List<string>();
+            foreach(int number in list)
+            {
+                stringList.Add(Transform(number));
+            }
+            return stringList;
+        }
+
+        public static List<string> Async(List<int> list)
+        {
+            int threadsCount = list.Count % 10 == 0 ? list.Count / 10 : list.Count / 10 + 1;
+            var fizzbuzzLists = new List<string>[threadsCount];
+
+            Parallel.For(0, threadsCount, (int threadNumber) => {
+                fizzbuzzLists[threadNumber] = Sync(list.Skip(threadNumber * 10).Take(10).ToList());
+            });
+            var resultList = new List<string>();
+            foreach(var anyNumber in fizzbuzzLists)
+            {
+                resultList.AddRange(anyNumber);
+            }
+            return resultList;
+        }      
+    }
+}
---
+++ fizzbuzz Async/fizzbuzzAsync_ConsoleApp/fizzbuzzAsync_ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fizbuuzAsync_ClassLibrary;
+
+namespace fizzbuzzAsync_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+
+            int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+            
+            var b = FizzBuzz.Sync(a.ToList());
+            var c = FizzBuzz.Async(a.ToList());            
+
+            foreach (string fizzbuzz in b)
+            {
+                Console.Write(fizzbuzz + " ");
+            }
+            Console.WriteLine();
+
+            foreach(string fizzbuzz in c)
+            {
+                Console.Write(fizzbuzz + " ");
+            }
+            Console.WriteLine();
+
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed.ToString(format: "g"));
+
+            Console.ReadKey();
+        }
+    }
+}
